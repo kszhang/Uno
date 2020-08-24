a986ea26dbcce48f172fd13d0854ae3175c004a2
Kevin Zhang <kevin@Kevins-MacBook-Pro.local>
2020-08-23 20:33:11 -0700

    Finished hard-coded bot

--- uno.py
+++ uno.py
@@ -5,29 +5,51 @@ import random
 
 # Gamestate
 class Game:
-  names = ["Bryan", "Raiden", "Erik", "Frank"]
+  names = ["Bryan", "Raiden", "Alan"]
 
-  def __init__(self):
+  def __init__(self, popOfBots):
     self.pile = []
     self.deck = Deck()
-    self.players = []
-    self.nPlayers = len(self.names)
+    avgTotal = 0
+    for k in popOfBots.botList:
+      # Start looping at this part for i in botList
+      self.players = []
+      self.nPlayers = len(self.names)
 
-    for i in range(self.nPlayers):
-      self.players.append(Player(self.names[i], i))
-
-    random.shuffle(self.deck.cards)
-
-    # Distribute cards to players
-    for n in range(7):
+      # do hard coded bots
       for i in range(self.nPlayers):
-        self.players[i].hand.append(self.deck.pop())
-        
-    # Starting card 
-    self.pile.insert(0, self.deck.pop())
+        self.players.append(Player(self.names[i], i))
 
-    # Game loop
-    self.gameloop()
+      # add the Learning Bot
+      self.players.append(k)
+      self.nPlayers = len(self.players)
+      # Now loop the game.
+      for j in xrange(1,100):
+        self.pile = []
+        self.deck = Deck()
+        random.shuffle(self.deck.cards)
+
+        # Clear player hands
+        for i in range(self.nPlayers):
+          self.players[i].hand = []
+
+        # Distribute cards to players
+        for n in range(7):
+          for i in range(self.nPlayers):
+            self.players[i].hand.append(self.deck.pop())
+            
+        # Starting card 
+        self.pile.insert(0, self.deck.pop())
+        # Do this a lot to find winrate
+        # Game loop
+        winner = self.gameloop()
+        if winner == k.name:
+          k.wins += 1
+      avgTotal += k.wins
+      print(k.name + ' won ' + str(k.wins) + ' out of 100')
+    avgTotal = avgTotal / len(popOfBots.botList)
+    print(str(avgTotal) + "% win average")
+
 
   def recycle(self):
     self.deck.cards = self.pile[0:len(self.pile)-1];
@@ -47,7 +69,7 @@ class Game:
     actionPerformed = False
     while (not won):
       if (toDraw):
-        print(self.players[turn].name + " drawing " + str(toDraw))
+        # print(self.players[turn].name + " drawing " + str(toDraw))
         for i in range(toDraw):
           self.players[turn].hand.append(self.deck.pop())
           if (len(self.deck.cards) == 0):
@@ -77,7 +99,13 @@ class Game:
         actionPerformed = True
         continue
         
-      decision = self.players[turn].takeTurn(self.legalMoves(self.players[turn].hand))
+      # Give other players number of cards
+      # set an "if" statement to check if the player is the 5th one to give different paramaters
+      decision = 0
+      if turn == 3:
+        decision = self.players[turn].takeTurn(self.legalMoves(self.players[turn].hand))
+      else:
+        decision = self.players[turn].takeTurn(self.legalMoves(self.players[turn].hand), self.players[turn].hand, self.pile, direction, turn, len(self.players[0].hand), len(self.players[1].hand), len(self.players[2].hand), len(self.players[3].hand))
       if (decision < 0):
         # Draw from deck
         self.players[turn].hand.append(self.deck.pop())
@@ -94,10 +122,12 @@ class Game:
         self.players[turn].hand.pop(decision)
 
         lastPlayed = self.pile[len(self.pile) - 1]
-        print(self.players[turn].name + " played " + lastPlayed.color + str(lastPlayed.rank))
+        # print(self.players[turn].name + " played " + lastPlayed.color + str(lastPlayed.rank))
 
         if (not self.players[turn].hand):
           won = True
+          # print(self.players[turn].name + " Won")
+          return self.players[turn].name
 
       turn = (turn + direction) % self.nPlayers
 
@@ -117,14 +147,106 @@ class Player:
     self.name = name
     self.number = number
     
+    #Method for hard coded bot
+  def hchoose(self, legalMoves, hand, pile, direction, currplayer, player1, player2, player3, player4):
+    if (not pile):
+      return -1
+    else:
+      toMove = legalMoves[0]
+      nextPlayer = currplayer + direction
+      nextHand = 0
+      if (nextPlayer == 0):
+        nextHand = player1
+      elif (nextPlayer == 1):
+        nextHand = player2
+      elif (nextPlayer == 2):
+        nextHand = player3
+      else:
+        nextHand = player4
+
+      top = pile[len(pile) - 1]
+      if top.rank == 12 or top.rank == 14:
+        for x in legalMoves:
+          if hand[x].rank == 12 and hand[x].rank == 14:
+            toMove = x
+            return toMove
+      
+      elif nextHand < 3:
+        for x in legalMoves:
+          if hand[x].rank > 9:
+            toMove = x
+            return toMove
+      
+      else:
+        # Counting colors in hand to find a card that can switch to ta better color
+        topColor = top.color
+        redCards = 0
+        yellowCards = 0
+        greenCards = 0
+        blueCards = 0
+        maxCount = 0
+        # If there's nothing but a wildcard, play it
+        maxColor = "w"
+        for x in hand:
+          if x.color == "r":
+            redCards += 1
+            if redCards > maxCount:
+              maxCount = redCards
+              maxColor = "r"
+          elif x.color == "y":
+            yellowCards += 1
+            if yellowCards > maxCount:
+              maxCount = yellowCards
+              maxColor = "y"
+          elif x.color == "g":
+            greenCards += 1
+            if yellowCards > maxCount:
+              maxCount = yellowCards
+              maxColor = "g"
+          elif x.color == "b":
+            blueCards += 1
+            if blueCards > maxCount:
+              maxCount = blueCards
+              maxColor = "b"
+          else:
+            # do nothing
+            maxCount = maxCount
+
+        for x in legalMoves:
+          if hand[x].color == maxColor:
+            toMove = x
+            return toMove
+
+      return toMove
+      # return legalMoves[0]
+
   # Returns -1 for draw, >= 0 index in hand to play
-  def takeTurn(self, legalMoves):
+  def takeTurn(self, legalMoves, hand, pile, direction, currplayer, player1, player2, player3, player4):
     if (not legalMoves):
       return -1
     else:
       # Make decision
-      return legalMoves[0]
+      answer = self.hchoose(legalMoves, hand, pile, direction, currplayer, player1, player2, player3, player4)
+      return answer
   
+
+class Bot(Player):
+  hand = []
+  def __init__(self, name, number, CNA):
+    Player.__init__(self, name, number)
+    self.CardValues = CNA
+    self.wins = 0
+
+  def takeTurn(self, legalMoves):
+    if (not legalMoves):
+      return -1
+    else:
+      return legalMoves[0]
+
+class Population:
+  def __init__(self):
+    self.botList = []
+
 # Card
 class Card:
   def __init__(self, color, rank):
@@ -149,4 +271,11 @@ class Deck:
     return self.cards.pop()
     
 if __name__ == "__main__":
-  Game()
+  # add command line stuff into here
+  botPop = Population()
+  for x in range(1,10):
+    botName = "Kevin-Jr-" + str(x)
+    DNA = []
+    botPop.botList.append(Bot(botName, 3, DNA))
+
+  Game(botPop)
